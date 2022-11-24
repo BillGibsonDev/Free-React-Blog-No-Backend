c1e7ddf7825b85096ea7d1c88ad07bfed17963bd
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-11-24 09:10:50 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,8 +5,8 @@ import * as palette from '../Theme.js';
 export const About = () => {
   return (
     <StyledAbout>
-        <h1>About Tech Blog</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi vitae ea facere hic natus animi eligendi expedita aut reiciendis numquam. Incidunt, modi. Cupiditate officia numquam dolorem est, exercitationem nostrum culpa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quibusdam accusamus rem quo atque enim? Culpa quia, incidunt eos minus sequi corporis, soluta repudiandae exercitationem magnam necessitatibus sunt quas odio!</p>
+      <h1>About Tech Blog</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi vitae ea facere hic natus animi eligendi expedita aut reiciendis numquam. Incidunt, modi. Cupiditate officia numquam dolorem est, exercitationem nostrum culpa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quibusdam accusamus rem quo atque enim? Culpa quia, incidunt eos minus sequi corporis, soluta repudiandae exercitationem magnam necessitatibus sunt quas odio!</p>
     </StyledAbout>
   )
 }
